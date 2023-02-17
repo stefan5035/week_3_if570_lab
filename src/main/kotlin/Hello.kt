ff0c38da9b31c4933bed7898586f183f3163a52c
stefan5035 <stefan.madethen@gmail.com>
2023-02-17 12:02:46 +0700

    Learning why (almost) everything has a value

--- src/main/kotlin/Hello.kt
+++ src/main/kotlin/Hello.kt
@@ -1,6 +1,26 @@
-fun main() {
-    // Will assign kotlin.Unit
-    val temperature = 10
-    val message = "The water temperature is ${ if (temperature > 50) "too warm" else "OK"}."
-    println(message)
+import java.util.*
+fun fishFood (day : String) : String {
+    val food : String
+    when (day) {
+        "Monday" -> food = "flakes"
+        "Wednesday" -> food = "redworms"
+        "Thursday" -> food = "granules"
+        "Friday" -> food = "mosquitoes"
+        "Sunday" -> food = "plankton"
+        else -> food = "nothing"
+    }
+    return food
+}
+fun feedTheFish() {
+    val day = randomDay()
+    val food = fishFood(day)
+    println ("Today is $day and the fish eat $food")
+}
+fun randomDay() : String {
+    val week = arrayOf ("Monday", "Tuesday", "Wednesday", "Thursday",
+        "Friday", "Saturday", "Sunday")
+    return week[Random().nextInt(week.size)]
+}
+fun main(args: Array<String>) {
+    feedTheFish()
 }
