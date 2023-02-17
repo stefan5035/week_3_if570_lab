11deb45ae4b7e196776981c86a9109e407143709
stefan5035 <stefan.madethen@gmail.com>
2023-02-17 12:03:34 +0700

    Learning more about functions

--- src/main/kotlin/Hello.kt
+++ src/main/kotlin/Hello.kt
@@ -1,25 +1,23 @@
-import java.util.*
+import java.util.* // required import
+fun randomDay() : String {
+    val week = arrayOf ("Monday", "Tuesday", "Wednesday", "Thursday",
+        "Friday", "Saturday", "Sunday")
+    return week[Random().nextInt(week.size)]
+}
 fun fishFood (day : String) : String {
-    val food : String
-    when (day) {
-        "Monday" -> food = "flakes"
-        "Wednesday" -> food = "redworms"
-        "Thursday" -> food = "granules"
-        "Friday" -> food = "mosquitoes"
-        "Sunday" -> food = "plankton"
-        else -> food = "nothing"
+    return when (day) {
+        "Monday" -> "flakes"
+        "Wednesday" -> "redworms"
+        "Thursday" -> "granules"
+        "Friday" -> "mosquitoes"
+        "Sunday" -> "plankton"
+        else -> "nothing"
     }
-    return food
 }
 fun feedTheFish() {
     val day = randomDay()
     val food = fishFood(day)
     println ("Today is $day and the fish eat $food")
-}
-fun randomDay() : String {
-    val week = arrayOf ("Monday", "Tuesday", "Wednesday", "Thursday",
-        "Friday", "Saturday", "Sunday")
-    return week[Random().nextInt(week.size)]
 }
 fun main(args: Array<String>) {
     feedTheFish()
