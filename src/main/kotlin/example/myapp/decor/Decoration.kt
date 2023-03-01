ba3c88f49dd2b5cf89999ef439796699b9339c48
stefan5035 <stefan.madethen@gmail.com>
2023-03-01 23:56:46 +0700

    Creating a data class

---
+++ src/main/kotlin/example/myapp/decor/Decoration.kt
@@ -0,0 +1,30 @@
+package example.myapp.decor
+
+/*data class Decoration(val rocks: String) {
+}
+fun makeDecorations() {
+    val decoration1 = Decoration("granite")
+    println(decoration1)
+    val decoration2 = Decoration("slate")
+    println(decoration2)
+    val decoration3 = Decoration("slate")
+    println(decoration3)
+    println (decoration1.equals(decoration2))
+    println (decoration3.equals(decoration2))
+}*/
+fun main(){
+    makeDecorations()
+}
+// Here is a data class with 3 properties.
+data class Decoration2(val rocks: String, val wood: String, val diver:
+String){
+}
+fun makeDecorations() {
+    val d5 = Decoration2("crystal", "wood", "diver")
+    println(d5)
+// Assign all properties to variables.
+    val (rock, wood, diver) = d5
+    println(rock)
+    println(wood)
+    println(diver)
+}
