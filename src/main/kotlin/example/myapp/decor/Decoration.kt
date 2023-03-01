c3fcb9523e2de69893bd6fa41f74ce0713de273f
stefan5035 <stefan.madethen@gmail.com>
2023-03-02 00:02:14 +0700

    Learning about singletons and enum

--- src/main/kotlin/example/myapp/decor/Decoration.kt
+++ src/main/kotlin/example/myapp/decor/Decoration.kt
@@ -19,6 +19,9 @@ fun main(){
 data class Decoration2(val rocks: String, val wood: String, val diver:
 String){
 }
+enum class Color(val rgb: Int) {
+    RED(0xFF0000), GREEN(0x00FF00), BLUE(0x0000FF);
+}
 fun makeDecorations() {
     val d5 = Decoration2("crystal", "wood", "diver")
     println(d5)
